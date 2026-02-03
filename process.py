a025abee4a2ab22ac5e60023190a4663b6ef1085
Brianna <brosem42@gmail.com>
2026-02-03 10:19:51 -0500

    Add evaluation of trained model: actor loss, critic loss, entropy loss

--- process.py
+++ process.py
@@ -122,4 +122,42 @@ def local_train(index, opt, global_model, optimizer, save=False):
                         end_time = timeit.default_timer()
                         print('Code runs in R%.2f ' % (end_time - start_time))
                     return
-    
+
+# we've deterimined 3 seperate losses: actor loss-encourages actions that lead to higher rewards,, critic loss-penalizes errors in value estimation, and entropy loss-encourages exploration by penalizing overly confident action distributions (penalizing strategies that are too greedy)
+# evaluation of trained model
+def local_test(index, opt, global_model):
+    torch.manual_seed(123 + index)
+    env, num_states, num_actions = create_train_env(opt.world, opt.stage, opt.action_type)
+    local_model = ActorCritic(num_states, num_actions)
+    local_model.eval()
+    state = torch.from_numpy(env.reset())
+    done = True
+    curr_step = 0
+    actions = deque(maxlen=opt.max_actions) #type:ignore
+    while True:
+        curr_step += 1
+        if done:
+            local_model.load_state_dict(global_model.state_dict())
+        with torch.no_grad():
+            if done:
+                h_0 = torch.zeros((1, 512), dtype=torch.float)
+                c_0 = torch.zeros((1, 512), dtype=torch.float)
+            else:
+                h_0 = h_0.detach()
+                c_0 = c_0.detach()
+        
+        logits, value, h_0, c_0 = local_model(state, h_0, c_0)
+        policy = F.softmax(logits, dim=1)
+        action = torch.argmax(policy).item()
+        state, reward, done, _ = env.step(action)
+        env.render()
+        actions.append(action)
+
+        if curr_step > opt.num_global_steps:
+            done = True
+        if done:
+            curr_step = 0
+            actions.clear()
+            state = env.reset()
+        state = torch.from_numpy(state)
+
